c1adb53033a56b1d07dd3adace968864af449605
MH Rose <mhasanrose888@gmail.com>
2023-06-03 20:02:33 +0600

    end the module

---
+++ array-split.js
@@ -0,0 +1,15 @@
+const nums = [1,2,3,4,5,6,7,8];
+// const part = nums.slice(2,5);
+
+const removed = nums.splice(2,3, 55, 65);
+// console.log(part);
+// console.log(removed);
+// console.log(nums);
+
+// const together = nums.join("");
+// const together = nums.join(" ");
+// const together = nums.join(", ");
+const together = nums.join(" ami, ");
+
+
+console.log(together);
